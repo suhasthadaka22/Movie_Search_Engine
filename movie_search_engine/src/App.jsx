85b4d432de08c2e3ea6b66bbd36b5b70cf642c0e
suhasthadaka22 <suhasthadaka2@gmail.com>
2025-05-11 10:44:57 +0530

    Update App.jsx

--- movie_search_engine/src/App.jsx
+++ movie_search_engine/src/App.jsx
@@ -3,20 +3,11 @@ import './App.css'
 function App() {
   return (
     <>
-      <Text display="First React Project!"/>
-      <div>
-        <p>Hello React</p>
-      </div>
-     </>  
+
+    </>  
   )
 }
 
-function Text({display}){
-  return (
-      <div>
-        <h1>{display}</h1>
-      </div>
-  );
-}
+
 
 export default App
